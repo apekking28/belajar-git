3ffd8e049c10f9694dfcabd762e2f14732213d6f
Ilham firmansyah <106460262+apekking28@users.noreply.github.com>
2023-03-15 15:14:31 +0700

    ketinggalan fungsinya perkaliannya

--- fungsi.js
+++ fungsi.js
@@ -5,14 +5,11 @@ console.log(add(1, 2));
 const subStarck = (a, b) => a - b;
 
 console.log(subStarck(1, 3));
-<<<<<<< HEAD
 
-const multyply = (a, b) => a * b;
+const multyplyNumber = (a, b) => a * b;
 
-console.log(multyply(2, 2));
+console.log(multyplyNumber(2, 2));
 
 const divide = (a, b) => a / b;
 
 console.log(divide(10, 2));
-=======
->>>>>>> parent of e93abdc (menambahkan fungsi perkalian)
